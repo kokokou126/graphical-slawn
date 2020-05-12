43bd98becf3577d36cfddefa4b78bd43f30c22ee
kokokou126 <kokokou126@gmail.com>
2020-05-12 10:07:25 +0900

    Edit files

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,111 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        graphical-slawn
-      </h1>
-      <h2 class="subtitle">
-        My dandy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div class="stack-area">
+      <ul class="stack-list">
+        <li v-for="item in stack" :key="item" class="stack-item">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+    <div class="input-area">
+      <div>
+        <input v-model="expression" autofocus class="input-expression" type="text" @keyup.enter="eval">
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Slawn from 'slawn'
+
+const slawn = new Slawn()
+slawn.eval(['hello', '!print'])
 
 export default {
-  components: {
-    Logo
+  components: {},
+  data () {
+    return {
+      expression: '',
+      stack: []
+    }
+  },
+  methods: {
+    eval () {
+      slawn.eval([parseInt(this.expression) || this.expression])
+      this.stack = [...slawn.stack].reverse()
+      this.expression = ''
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="stylus">
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap')
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+@keyframes dropdown
+  from
+    opacity 0
+    transform translateY(-100%)
+  to
+    opacity 1
+    transform translateY(0)
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+body
+  font-size: 1.6rem
+  margin 0
+  padding 0
 
-.links {
-  padding-top: 15px;
-}
+html
+  font-size 62.5%
+
+.container
+  display grid
+  grid-template-columns 1fr max-content 1fr
+  grid-template-rows 50% 50%
+  grid-template-areas \
+    ". stack ." \
+    ". input ."
+  height 100vh
+  margin 0
+  width 100vw
+
+.input-area
+  bottom 0
+  grid-area input
+  height max-content
+  left 0
+  margin auto
+  right 0
+  top 0
+  width 100%
+
+.stack-area
+  bottom 0
+  grid-area stack
+  height max-content
+  left 0
+  margin auto
+  overflow auto
+  right 0
+  top 0
+  width 100%
+
+.stack-item
+  animation-duration .5s
+  animation-name dropdown
+  border 1px solid #dddddd
+  padding 10px
+
+.stack-list
+  font-family Inconsolata, monospace
+  font-size 1.8rem
+  list-style none
+  padding-left 0
+
+.input-expression
+  border none
+  border-bottom 1px solid
+  font-family Inconsolata, monospace
+  font-size 1.8rem
+  outline none
 </style>
